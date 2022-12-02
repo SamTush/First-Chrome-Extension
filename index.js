bd1009cc233acd1cacb7237631a6d22141f4734c
samson <samo4421@icloud.com>
2022-12-02 13:44:41 +0300

    about to deploy

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+let saveEl = [];
 let saveButton = document.querySelector("#clicked");
+let inputEl = document.querySelector("#input-el");
+let ulEL = document.querySelector("#ul-el");
 
 saveButton.addEventListener("click", function () {
-    console.log("button clicked");
+    saveEl.push(inputEl.value);
+    renderSaves();
+    clearText();
 });
+
+function renderSaves() {
+    let listItem = "";
+
+    for (let i = 0; i < saveEl.length; i++) {
+       //listItem += '<li class="list-group-item list-group-item-secondary"><a href="' + saveEl[i] + '" target="_blank">' + saveEl[i] + "</a></li>"  
+       listItem += `
+            <li class="list-group-item list-group-item-secondary">
+                <a href='${saveEl[i]}' target='_blank'>
+                    ${saveEl[i]}
+                </a>
+            </li>
+        `
+
+    }
+
+    ulEL.innerHTML = listItem;
+
+}
+
+function clearText() {
+    inputEl.value = "";
+}
+
+
+
